e97dbd7c8dceb8f86f9bfe76da9da5982c5d03e0
mredivo9 <168040123+mredivo9@users.noreply.github.com>
2024-09-02 22:17:37 -0300

    Add files via upload

---
+++ VerificaParOuImpar.java
@@ -0,0 +1,14 @@
+public class VerificaParOuImpar {
+    public static void main(String[] args) {
+
+    int numero = 29; //
+
+
+    if (numero % 2 == 0) {
+        System.out.println("O número " + numero + " é par.");
+    } else {
+        System.out.println("O número " + numero + " é ímpar.");
+    }
+}
+
+}
